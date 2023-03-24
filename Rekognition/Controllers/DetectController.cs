f932b78520b45f7dbd40fcd206d5f34ee128228d
Ahmed Mekkawi <ahmed.mekkawi@hotmail.com>
2023-03-23 22:05:41 -0600

    Add project files.

---
+++ Rekognition/Controllers/DetectController.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Rekognition.Model;
+using System;
+using System.Web.Http;
+
+namespace Rekognition.Controllers
+{
+    public class DetectController : ApiController
+    {
+
+        [HttpPost]
+        public DetectFacesResponse Detect([FromBody] string imageData)
+        {
+            DetectFacesResponse response = null;
+            if (imageData != null)
+            {
+                var imageBytes = Convert.FromBase64String(imageData);
+
+                DetectFaces df = new DetectFaces();
+                response = df.Detectface(imageBytes);
+            }
+            return response;
+        }
+
+    }
+}
---
+++ Rekognition/DetectFaces.cs
@@ -0,0 +1,44 @@
+﻿using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Rekognition
+{
+    public class DetectFaces
+    {
+        private const string accessKeyID = ""; // use your accessKeyId 
+        private const string secretKey = ""; // use your secretKey 
+
+        public DetectFacesResponse Detectface(byte[] imageBytes)
+        {
+            var credentials = new BasicAWSCredentials(accessKeyID, secretKey);
+
+            AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(credentials, RegionEndpoint.USEast2);
+            DetectFacesResponse detectFacesResponse = null;
+            try
+            {
+                MemoryStream ms = new MemoryStream(imageBytes);
+                DetectFacesRequest detectFacesRequest = new DetectFacesRequest()
+                {
+                    Image = new Image()
+                    {
+                        Bytes = ms
+                    },
+                    Attributes = new List<String>() { "ALL" }
+                };
+
+                detectFacesResponse = rekognitionClient.DetectFaces(detectFacesRequest);
+            }
+            catch (AmazonRekognitionException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return detectFacesResponse;
+        }
+    }
+}
